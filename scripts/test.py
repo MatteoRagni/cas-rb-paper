e04305bb56da55b5400ab10c03fd6541eb57ea9c
MatteoRagni <matteo.ragni.it@gmail.com>
2016-10-27 20:14:53 +0200

    corrections to last written part

--- scripts/test.py
+++ scripts/test.py
@@ -4,25 +4,34 @@ import math
 def integrate(f, a, b, n):
     h = (b - a)/n
     x = sympy.symbols('x')
-
     func = sympy.lambdify((x), f)
 
-    sums = (func(a) + func(b)) / 2.0
+    sums = (func(a) +
+            func(b)) / 2.0
+
     for i in range(1, n):
-        sums += func(a + i * h)
+        sums += func(a + i*h)
+
     return sums * h
+
 
 def order(f, a, b, n):
     x = sympy.symbols('x')
 
-    f_ab = sympy.Subs(f, (x), (b)).n() - sympy.Subs(f, (x), (a)).n()
-    f_1n = integrate(f.diff(x), a, b, n)
-    f_2n = integrate(f.diff(x), a, b, 2 * n)
+    f_ab = sympy.Subs(f, (x), (b)).n()-\
+           sympy.Subs(f, (x), (a)).n()
+    df   = f.diff(x)
+    f_1n = integrate(df, a, b, n)
+    f_2n = integrate(df, a, b, 2 * n)
 
-    return math.log((f_ab - f_1n)/(f_ab - f_2n), 2)
+    return math.log(
+      (f_ab - f_1n) /
+      (f_ab - f_2n),
+    2)
+
 
 x = sympy.symbols('x')
 f = sympy.atan(x)
 
-o = order(f, -1.0, 1.0, 100)
-print(o)
+print(order(f, -1.0, 1.0, 100))
+# => 1.9999999974244451
--- scripts/test.rb
+++ scripts/test.rb
@@ -1,13 +1,16 @@
 require 'ragni-cas'
 
+
 def integrate(f, a, b, n)
   h = (b - a) / n
 
   func = f.as_proc
 
-  sum = ((func.call 'x' => a) + (func.call 'x' => b)) / 2.0
+  sum = ((func.call 'x' => a) +
+        (func.call 'x' => b)) / 2.0
+
   for i in (1...n)
-    sum += (func.call 'x' => (a + i * h))
+    sum += (func.call 'x' => (a + i*h))
   end
   return sum * h
 end
@@ -15,15 +18,20 @@ end
 def order(f, a, b, n)
   x = CAS.vars 'x'
 
-  f_ab = (f.call x => b) - (f.call x => a)
-  f_1n = integrate(f.diff(x).simplify, a, b, n)
-  f_2n = integrate(f.diff(x).simplify, a, b, 2 * n)
+  f_ab = (f.call x => b) -
+         (f.call x => a)
+  df   = f.diff(x).simplify
+  f_1n = integrate(df, a, b, n)
+  f_2n = integrate(df, a, b, 2 * n)
 
-  return Math.log((f_ab - f_1n)/(f_ab - f_2n), 2)
+  return Math.log(
+    (f_ab - f_1n) /
+    (f_ab - f_2n),
+  2)
 end
 
 x = CAS.vars 'x'
 f = CAS.arctan x
 
-o = order f, -1.0, 1.0, 100
-puts o
+puts(order f, -1.0, 1.0, 100)
+# => 1.9999999974244451
