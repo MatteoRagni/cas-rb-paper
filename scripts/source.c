ecce368db25cd26c0d4da2364a839cdf33c1dee6
MatteoRagni <matteo.ragni.it@gmail.com>
2016-11-26 20:10:32 +0100

    Moved CLib into examples

---
+++ scripts/source.c
@@ -0,0 +1,21 @@
+// Source file for library: example.c
+
+#include "example.h"
+
+double f_impl(double x, double y) {
+  double __t_0 = pow(x, y);
+  double __t_1 = g_impl(x);
+  double __t_2 = sin(__t_0);
+  double __t_3 = log(__t_2);
+  double __t_4 = (__t_1 + __t_3);
+  double __t_5 = (__t_0 + __t_4);
+
+  return __t_5;
+}
+
+// end of example.c
+
+
+
+
+;
---
+++ scripts/source.h
@@ -0,0 +1,21 @@
+// Header file for library: example.c
+
+#ifndef example_H
+#define example_H
+
+// Standard Libraries
+#include <math.h>
+
+// Local Libraries
+#include "g_impl"
+
+// Definitions
+#define M_PI 3.141592653589793
+#define M_INFINITY HUGE_VAL
+#define M_E 2.718281828459045
+#define M_EPSILON 1.0e-16
+
+// Functions
+double f_impl(double x, double y);
+
+#endif // example_H
